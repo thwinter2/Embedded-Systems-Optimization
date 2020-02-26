d249ab92fe15ef562aba3194d390c5d3b0a842ae
Alex Dean <agdean@ncsu.edu>
2020-02-26 15:53:14 -0500

    Added debug bit comments

--- Project1/Include/debug.h
+++ Project1/Include/debug.h
@@ -5,13 +5,13 @@
 #define MASK(x) (1UL << (x))
 
 // Debug Signals on port B
-#define DBG_1 1 	
-#define DBG_2 2	  
-#define DBG_3 3	
-#define DBG_4 8		
-#define DBG_5 9		
-#define DBG_6 10  
-#define DBG_7 11
+#define DBG_1 1 	// SPI_RW
+#define DBG_2 2	  // SD_Read
+#define DBG_3 3		// SD_Write
+#define DBG_4 8		// pf_read
+#define DBG_5 9		// SD_Init
+#define DBG_6 10	// unused
+#define DBG_7 11	// unused
 
 #define DEBUG_START(channel) {PTB->PSOR = MASK(channel);} 
 #define DEBUG_STOP(channel) { PTB->PCOR = MASK(channel); }
